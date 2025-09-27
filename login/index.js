5f18e969549abe679b3a0f48477a0d90b8241926
Daria <dshyrmovska@gmail.com>
2025-09-27 22:11:30 +0200

    more js events

--- login/index.js
+++ login/index.js
@@ -23,6 +23,9 @@
 // }
 function buttonHandler() {
     console.log("button");
+    const demo = document.getElementById("demo");
+    demo.innerText = "button was clicked";
+    demo.style.color = "purple";
 }
 function loginHandler() {
     const loginValue = document.getElementById("login").value;
@@ -32,4 +35,29 @@ function loginHandler() {
 
     // login.innerHTML = "login: " + loginValue;
     console.log("Value: " + loginValue);
+}
+function passwordHandler() {
+    const passwordBox = document.getElementById("passwordBox");
+    const passwordValue = document.querySelector(".passwordInput").value;
+    const h2 = document.createElement("h2");
+    h2.innerText = `password: ${passwordValue}`;
+    // if(h2.value = 0){
+
+    // }else{
+
+    // }
+    console.log(h2.textContent.length);
+
+    passwordBox.innerHTML = h2;
+    const boxWrapper = document.getElementsByClassName("boxWrapper")[0];
+    console.log(boxWrapper);
+    boxWrapper.appendChild(h2);
+
+    //1. положить что-то в х2  2. проверить что лежит в х2  3. куда то положить х2(3.1 положить в демо 3.2 положить в boxWrapper)
+
+    // h2.innerText = "lala";
+    console.log(h2);
+
+    //prompt alert
+    // prompt("password value: " + passwordValue);
 }
