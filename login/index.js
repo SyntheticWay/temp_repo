fc49acaaf6a12250fc13a848f28cf1672c850802
Daria <dshyrmovska@gmail.com>
2025-09-27 19:14:04 +0200

    temp commit

---
+++ login/index.js
@@ -0,0 +1,11 @@
+// console.log(document.getElementById("input1").value);
+// console.log(document.getElementById("input2").value);
+
+// onclick = "console.log(document.querySelector('input1').value)";
+
+function lala() {
+    const inputt1 = document.getElementById("input1").value;
+    const inputt2 = document.getElementById("input2").value;
+    document.getElementById("lala").innerHTML = "lala: " + inputt1;
+    document.getElementById("lala").innerHTML = "lala: " + inputt2;
+}
