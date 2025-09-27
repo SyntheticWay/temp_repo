9d6dc67d113802e3d2eb069e48b63e62351343f0
Daria <dshyrmovska@gmail.com>
2025-09-27 21:15:32 +0200

    js events

--- login/index.js
+++ login/index.js
@@ -2,12 +2,34 @@
 // console.log(document.getElementById("input2").value);
 
 // onclick = "console.log(document.querySelector('input1').value)";
+//задание 1
+//обратиться к элементу через addEventListener и в скрипте на хтмл
+//задание 2
+// если значение логина !=0, тогда положить значение в коробку
+// если значение пароля !=0, тогда положить значение в коробку
+// по дефолту - console.log('nothing is here');
+// function lala() {
+//     // value
+//     const loginValue = document.getElementById("input1").value;
+//     const passwordValue = document.getElementById("input2").value;
+//     //box
+//     const login = document.getElementById("login");
+//     const password = document.getElementById("password");
 
-function lala() {
-    const inputt1 = document.getElementById("input1").value;
-    const inputt2 = document.getElementById("input2").value;
-    document.getElementById("lalal").innerHTML = "lala: " + inputt1;
-    document.getElementById("lalal2").innerHTML = "lala: " + inputt2;
-    console.log(inputt1);
-    console.log(inputt2);
+//     login.innerHTML = "login: " + loginValue;
+//     password.innerHTML = "password: " + passwordValue;
+//     console.log("login: ", loginValue);
+//     console.log("password: ", passwordValue);
+// }
+function buttonHandler() {
+    console.log("button");
+}
+function loginHandler() {
+    const loginValue = document.getElementById("login").value;
+    const loginBox = document.getElementById("loginBox");
+
+    loginBox.innerHTML = "login: " + loginValue;
+
+    // login.innerHTML = "login: " + loginValue;
+    console.log("Value: " + loginValue);
 }
